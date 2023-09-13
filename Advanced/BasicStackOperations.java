3fbcc69c5f2ebb2d1059244615262b20a0617f42
Veselin Vachkov <125015346+veselinvachkov@users.noreply.github.com>
2023-09-13 19:43:56 +0300

    Add files via upload

---
+++ Advanced/BasicStackOperations.java
@@ -0,0 +1,11 @@
+package Advanced;
+
+import java.util.Scanner;
+
+public class BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
